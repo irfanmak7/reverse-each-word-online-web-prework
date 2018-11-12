38d7fcb5b9f06b6c15b776b214ed3dec7ed09b8b
Irfan Maknojia <irfan.mak7@gmail.com>
2018-11-12 23:24:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ def reverse_each_word(string)
   new_array = string.split(" ")
   array1 = []
   new_array.collect do |string|
-    string.reverse
+    return string.reverse
   end
-  return string.join(" ")
 end
