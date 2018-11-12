212a607d1ce0929769957b757bb858d5e71069f4
Irfan Maknojia <irfan.mak7@gmail.com>
2018-11-12 23:27:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
   new_array = string.split(" ")
   array1 = []
   new_array.collect do |word|
-    word.reverse
+    array1.push(word.reverse)
   end
+array1.join(" ")
 end
